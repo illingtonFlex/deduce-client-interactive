55d7891d7a518df541bc96b2bc22f5be852fb255
illingtonFlex <jason.norman@gmail.com>
2019-06-11 20:39:08 -0500

    Rudimentary learning.

---
+++ src/main/kotlin/LettersGridView.kt
@@ -0,0 +1,141 @@
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class LettersGridView : View() {
+    override val root = gridpane {
+        row {
+            button("0") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("1") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("2") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("3") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("4") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("5") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("6") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+        }
+        row {
+            button("7") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("8") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("9") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("10") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("11") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("12") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("13") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+        }
+        row {
+            button("14") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("15") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("16") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("17") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("18") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("19") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+            button("20") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                }
+            }
+        }
+    }
+}
