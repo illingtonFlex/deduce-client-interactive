34d965892aa64aebdb2b00139ea2bf8d0306a862
jason.norman <jason.norman@quikq.com>
2019-06-12 12:09:50 -0500

    Slightly less rudimentary learning.

--- src/main/kotlin/InteractiveClientApp.kt
+++ src/main/kotlin/InteractiveClientApp.kt
@@ -1,8 +1,20 @@
+package jcn.deduce.client
+
 import javafx.stage.Stage
 import jcn.deduce.client.view.MainView
 import tornadofx.App
 
 class InteractiveClientApp : App(MainView::class) {
+    companion object {
+        var knownLetters = arrayOfNulls<String>(21)
+    }
+
+    override fun init() {
+        knownLetters[4] = "T"
+        knownLetters[9] = "G"
+        knownLetters[17] = "Z"
+    }
+
     override fun start(stage: Stage) {
         stage.minHeight = 200.0
         stage.minWidth = 400.0
--- src/main/kotlin/jcn/deduce/client/view/LettersGridView.kt
+++ src/main/kotlin/jcn/deduce/client/view/LettersGridView.kt
@@ -1,138 +1,29 @@
 package jcn.deduce.client.view
 
 import tornadofx.*
+import jcn.deduce.client.InteractiveClientApp.Companion.knownLetters
+
 
 class LettersGridView : View() {
     override val root = gridpane {
-        row {
-            button("0") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
+
+        knownLetters.forEachIndexed { index, element ->
+
+            if (index == 0 || index % 7 == 0) {
+                row {
+                    button(element?.toString() ?: index.toString()) {
+                        useMaxWidth = true
+                        gridpaneConstraints {
+                            marginBottom = 10.0
+                        }
+                    }
                 }
-            }
-            button("1") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("2") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("3") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("4") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("5") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("6") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-        }
-        row {
-            button("7") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("8") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("9") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("10") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("11") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("12") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("13") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-        }
-        row {
-            button("14") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("15") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("16") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("17") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("18") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("19") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
-                }
-            }
-            button("20") {
-                useMaxWidth = true
-                gridpaneConstraints {
-                    marginBottom = 10.0
+            } else {
+                button(element?.toString() ?: index.toString()) {
+                    useMaxWidth = true
+                    gridpaneConstraints {
+                        marginBottom = 10.0
+                    }
                 }
             }
         }
