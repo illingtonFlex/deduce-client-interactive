10426fe93cd0f3109f3b07bccf3d3e2b5c2eedd6
illingtonFlex <jason.norman@gmail.com>
2019-06-12 21:30:09 -0500

    Rudimentary learning.

--- src/main/kotlin/jcn/deduce/client/view/LettersGridView.kt
+++ src/main/kotlin/jcn/deduce/client/view/LettersGridView.kt
@@ -5,24 +5,35 @@ import jcn.deduce.client.InteractiveClientApp.Companion.knownLetters
 
 
 class LettersGridView : View() {
+    companion object {
+        const val NUM_OF_ROWS = 3
+    }
+
     override val root = gridpane {
 
-        knownLetters.forEachIndexed { index, element ->
+        val rowSize = knownLetters.size/NUM_OF_ROWS
 
-            if (index == 0 || index % 7 == 0) {
-                row {
-                    button(element?.toString() ?: index.toString()) {
+        val rows = knownLetters.toList().chunked(rowSize)
+
+        rows.forEachIndexed { rowIndex, elements ->
+            row {
+
+                elements.forEachIndexed { buttonIndex, element ->
+                    val displayIndex = rowSize * rowIndex + buttonIndex
+
+                    button("${element ?: displayIndex}") {
+
                         useMaxWidth = true
+
+                        action {
+                            this.isDisable = true
+                            this.text = "X"
+                        }
+
                         gridpaneConstraints {
                             marginBottom = 10.0
+
                         }
-                    }
-                }
-            } else {
-                button(element?.toString() ?: index.toString()) {
-                    useMaxWidth = true
-                    gridpaneConstraints {
-                        marginBottom = 10.0
                     }
                 }
             }
